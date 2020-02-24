b295672afe2f3f74e508f28a5adf28f60eee7604
manulangat <emmanuelthedeveloper@gmail.com>
2020-02-24 13:50:11 +0300

    debugging the frontend

---
+++ frontend/src/components/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+import ReactDOM from 'react-dom';
+
+
+
+class App extends React.Component {
+render() {
+   return (
+       <div>
+            <h1>hey</h1>
+       </div>
+   )
+}
+}
+
+
+ReactDOM.render(<App />, document.getElementById('App'));
--- taxi/urls.py
+++ taxi/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import path,include
 
 urlpatterns = [
-    path('',include('backend.urls')),
+    path('',include('frontend.urls')),
+    path('api/',include('backend.urls')),
     path('admin/', admin.site.urls),
 ]
